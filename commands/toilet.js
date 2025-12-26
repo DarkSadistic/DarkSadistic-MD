cc47529be7043e9a3e4b61c09f82360b6153ac44
DarkSadistic <darksadistic55@gmail.com>
2025-12-26 07:31:45 +0500

    Create toilet.js

---
+++ commands/toilet.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "toilet",
+  description: "Funny command from DarkSadistic-MD",
+  category: "fun",
+  async run({ conn, m, args }) {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const messages = ['🚽 Flushing user…', 'Loading... 99%... SPLASH!', 'You have been flushed by No one. Please dry yourself before continuing.'];
+    for (const msg of messages) {
+      await delay(2000);
+      await conn.sendMessage(m.chat, { text: msg }, { quoted: m });
+    }
+  }
+};
+                             
